952954c1a1bb1d8e88fae7a646822d1cbfa30095
haniya akhtar <thehaniyaakhtar@gmail.com>
2026-04-28 13:43:54 +0530

    im2col

--- draft1.py
+++ draft1.py
@@ -58,3 +58,100 @@ def pad2d(X, pad):
     )
     return X_padded
     # return padded image
+    
+# for input (1, 3, 5, 5), pad = 1
+# padddinf adds +2 to height (top + bottom)
+# padding adds +2 to width (left + right)
+'''
+1 2 3
+4 5 6
+7 8 9
+
+padding:
+0 0 0 0 0
+0 1 2 3 0
+0 4 5 6 0
+0 7 8 9 0
+0 0 0 0 0
+'''
+
+# Im2Col
+
+'''
+image:
+1 2 3
+4 5 6
+7 8 9
+
+small box:
+1 0
+0 1
+
+Goal: produce a smaller image
+
+Processed in parts:
+
+Image   Kernel:
+1 2     1 0
+4 5     0 1
+
+Multiplied:
+(1×1) + (2×0) + (4×0) + (5×1)
+= 1 + 0 + 0 + 5
+= 6
+One number in output
+
+Moved the box to right, then down
+this process is not possible to code:
+nested loops are slow
+hard to scale
+pain for backprop
+
+this im2col
+Step 1: Each row = one patch
+[1 2 4 5]
+[2 3 5 6]
+[4 5 7 8]
+[5 6 8 9]
+
+Step 2: 
+Flatten Kernel
+[1 0 0 1]
+
+Step 3: Multiply  in one shot
+matrix(4x4) x vector(4x1) = (4x1)
+'''
+
+def im2col(X, k):
+    # X is a 2d image (H, W)
+    # k is kernel size
+    H, W = X.shape
+    # extract H and W of image
+    
+    patches = []
+    # expty list to store all patches
+    
+    # slide window vertically
+    for i in range(H - k + 1):
+        # slide window horizontally
+        # H-k+1 as pach must fit inside image
+        for j in range(W - k + 1):
+            # extract k x k patch
+            # controls left to right sliding
+            patch = X[i:i+k, j:j+k]
+            # slicing from i/j to i+k/j+k
+            
+            # flatten to 1D
+            patch_flat = patch.flatten()
+            
+            
+            #store
+            patches.append(patch_flat)
+            
+    return np.array(patches)
+    # convert patch to array
+    
+# image = 5 x 5
+# kernel = 3 x 3 
+# 5 - 3 + 1 = 3; 3 vert and hor posiitons
+# 3 x 3 = 9 rows in im2col output
